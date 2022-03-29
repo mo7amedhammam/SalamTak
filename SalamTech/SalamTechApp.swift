1da7f8981929db13ad4e9e9db4e956493087e5e9
Mostafa Morsy <mostafa.morsy@msa.edu.eg>
2022-03-29 03:03:14 -0700

    Initial Commit

---
+++ SalamTech/SalamTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalamTechApp.swift
+//  SalamTech
+//
+//  Created by wecancity agency on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SalamTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
