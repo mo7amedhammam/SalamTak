668c615efaae16df09f0c5e63581217e72e0f4e8
mohamed hammam <mohamed.hammam.pro@gmail.com>
2022-04-17 17:27:42 +0200

    late 17/4

--- SalamTech/SalamTechModules/HomeModule/TabBarModule/BookingModule/Summary/ViewSummary.swift
+++ SalamTech/SalamTechModules/HomeModule/TabBarModule/BookingModule/Summary/ViewSummary.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ViewSummary:View{
     var Doctor:Doc
+    @State var showQuickLogin = false
 
     var body: some View{
 //        NavigationView{
@@ -21,11 +22,15 @@ struct ViewSummary:View{
                 
 
 //                        ViewDocCell(Doctor: Doctor,searchDoc: searchDoc,gotodoctorDetails:$gotodoctorDetails,SelectedDoctor:$SelectedDoctor )
-                        
+                        Spacer()
                     
+                ButtonView(text:"Sign in", action: {
+                    withAnimation(.easeIn(duration: 0.3)) {
+                        showQuickLogin =  true
+                    }
+                })
                     
-                    
-                
+             
               
                 Spacer()
             }
@@ -42,6 +47,44 @@ struct ViewSummary:View{
             }
             .edgesIgnoringSafeArea(.vertical)
             
+            
+//            if showQuickLogin{
+            ConfirmBooking(IsPresented: $showQuickLogin, content: {
+                    HStack{
+                        VStack() {
+                            Text("230")                                .font(.system(size: 18))
+//                                .padding(.top)
+
+                            Text("\nEGP")
+                                .font(.system(size: 15))
+                                .foregroundColor(.black.opacity(0.5))
+//                                .padding(.bottom,-10)
+
+                        }.padding(.leading)
+                            .padding(.top)
+                        Button(action: {
+                            // add review
+                            showQuickLogin =  true
+                        }, label: {
+                            HStack {
+                                Text("Confirm")
+                                    .fontWeight(.semibold)
+                                    .font(.title2)
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color("blueColor"))
+            //                .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(12)
+                            .padding(.horizontal, 20)
+                        })
+                    }.padding(.horizontal)
+                    
+                })
+    
+//            }
+  
         }
             .edgesIgnoringSafeArea(.top)
             .onAppear(perform: {
@@ -66,6 +109,8 @@ struct ViewSummary:View{
 
             })
         
+        
+       
         
 //     }
         
@@ -92,3 +137,63 @@ struct ViewSummary_Previews: PreviewProvider {
     }
 }
 
+
+
+
+
+struct ConfirmBooking <Content: View>: View {
+
+    let content: Content
+    var language : Language
+      var IsPresented: Binding<Bool>
+
+    init(  IsPresented:Binding<Bool>, @ViewBuilder content: () -> Content) {
+        self.language = LocalizationService.shared.language
+        self.IsPresented = IsPresented
+        self.content = content()
+    }
+
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            ZStack {
+                
+              
+                
+                VStack {
+                        Capsule()
+                            .frame(width: 50, height: 4)
+                            .foregroundColor(.gray)
+                            .padding(.top ,10)
+                            .padding(.bottom,20)
+                    
+                    VStack {
+                        self.content
+                    }
+
+                }.background(
+                    RoundedRectangle(cornerRadius: 40.0)
+                        .foregroundColor(.white)
+                        .ignoresSafeArea()
+                        .opacity(1.5)
+                        .shadow(radius: 15)
+                        .frame(width: UIScreen.main.bounds.width)
+
+)
+
+            }
+
+        }
+        .frame(width: UIScreen.main.bounds.width)
+//        .background(
+//            Color.black.opacity(0.3)
+//                .blur(radius: 12)
+//        )
+        .onTapGesture(perform: {
+            IsPresented.wrappedValue.toggle()
+
+        })
+        
+    }
+}
