064dfa01c25860c8440cb339fc5b95a94f451b49
mohamed hammam <mohamed.hammam.pro@gmail.com>
2022-06-06 17:59:13 +0200

    late 6/6

--- SalamTech/SalamTechModules/HomeModule/TabBarModule/BookingModule/SearchDoctor/doctor/ViewModel/VMSearchDoc.swift
+++ SalamTech/SalamTechModules/HomeModule/TabBarModule/BookingModule/SearchDoctor/doctor/ViewModel/VMSearchDoc.swift
@@ -15,10 +15,11 @@ class VMSearchDoc: ObservableObject {
     let passthroughSubject = PassthroughSubject<String, Error>()
     let ModelFetchDoctors = PassthroughSubject<BaseResponse<ModelDoc<[Doc]>> , Error>()
     private var cancellables: Set<AnyCancellable> = []
-    // ------- input
+
     @Published var searchDocOperation : searchDocType = .fetchDoctors
     @Published var publishedModelSearchDoc: [Doc] = [Doc.init( id: 7878787878787, FeesFrom: 78787878787, DoctorName: "", SubSpecialistName: [], MedicalExamationTypeImage: [])]
     
+    //MARK: --- inputs ------
     @Published var MaxResultCount                      :Int = 0
     @Published var SkipCount                            :Int = 0
     @Published var SpecialistId                         :Int = 0
@@ -31,7 +32,7 @@ class VMSearchDoc: ObservableObject {
     @Published var SeniortyLevelId                      :Int? = 0
     @Published var SubSpecialistId                      :[Int]? = []
     
-//MARK: --- Filter ------
+    //MARK: --- Filter ------
     @Published var FilterSpecialistId                        :Int? = 0
     @Published var FilterCityId                               :Int? = 0
     @Published var FilterAreaId                               :Int? = 0
@@ -39,15 +40,14 @@ class VMSearchDoc: ObservableObject {
     @Published var FilterFees                                  :Int? = 0
     @Published var FilterSeniortyLevelId                     :Int? = 0
     @Published var FilterSubSpecialistId                     :[Int]? = []
- 
+    
     //------- output
     @Published var noDoctors = false
-
     @Published var isLoading:Bool? = false
     @Published var isAlert = false
     @Published var activeAlert: ActiveAlert = .NetworkError
     @Published var message = ""
-
+    
     init() {
         ModelFetchDoctors.sink { (completion) in
             //            print(completion)
@@ -70,132 +70,124 @@ class VMSearchDoc: ObservableObject {
             case .none:
                 break
             }
-            
         }.store(in: &cancellables)
-    
+        
     }
- 
 }
-
 
 enum searchDocType{
     case fetchDoctors, fetchMoreDoctors
 }
 
 extension VMSearchDoc:TargetType{
-
+    
     var url: String{
         switch searchDocOperation{
             
         case .fetchDoctors:
-       return URLs().DoctorSearch
-                
+            return URLs().DoctorSearch
+            
         case .fetchMoreDoctors:
             return URLs().DoctorSearch
         }
     }
     
     var method: httpMethod{
-            return .Post
+        return .Post
     }
     
     var parameter: parameterType{
         var Parameters : [String:Any] = [
-                // required
-                    "MaxResultCount": MaxResultCount ,
-                    "SkipCount":SkipCount,
-                    "SpecialistId":FilterSpecialistId == 0 ? SpecialistId:FilterSpecialistId ?? true ,
-                    "MedicalExaminationTypeId":MedicalExaminationTypeId
-            ]
-                // optional
-                if DoctorName != ""{
-                    Parameters["DoctorName"] = DoctorName
-                }
-                
-                if FilterCityId != 0{
-                    Parameters["CityId"] = FilterCityId
-                }else if CityId != 0{
-                    Parameters["CityId"] = CityId
-                }
-                
-                if FilterAreaId != 0{
-                    Parameters["AreaId"] = FilterAreaId
-                }else  if AreaId != 0{
-                    Parameters["AreaId"] = AreaId
-                }
-                
-                if FilterFees != 0{
-                    Parameters["Fees"] = FilterFees
-                }else if Fees != 0{
-                    Parameters["Fees"] = Fees
-                }
-                
-                if FilterGenderId != 0{
-                    Parameters["GenderId"] = FilterGenderId
-                }else if GenderId != 0{
-                    Parameters["GenderId"] = GenderId
-                }
-                
-                if FilterSeniortyLevelId != 0{
-                    Parameters["SeniortyLevelId"] = FilterSeniortyLevelId
-                }else if SeniortyLevelId != 0{
-                    Parameters["SeniortyLevelId"] = SeniortyLevelId
-                }
-                
-                if FilterSubSpecialistId != []{
-                    Parameters["SubSpecialistId"] = FilterSubSpecialistId
-                }else if SubSpecialistId != []{
-                    Parameters["SubSpecialistId"] = SubSpecialistId
-                }
-            return .parameterRequest(Parameters: Parameters, Encoding: JSONEncoding.default)
-
+            // required
+            "MaxResultCount": MaxResultCount ,
+            "SkipCount":SkipCount,
+            "SpecialistId":FilterSpecialistId == 0 ? SpecialistId:FilterSpecialistId ?? true ,
+            "MedicalExaminationTypeId":MedicalExaminationTypeId
+        ]
+        // optional
+        if DoctorName != ""{
+            Parameters["DoctorName"] = DoctorName
+        }
+        
+        if FilterCityId != 0{
+            Parameters["CityId"] = FilterCityId
+        }else if CityId != 0{
+            Parameters["CityId"] = CityId
+        }
+        
+        if FilterAreaId != 0{
+            Parameters["AreaId"] = FilterAreaId
+        }else  if AreaId != 0{
+            Parameters["AreaId"] = AreaId
+        }
+        
+        if FilterFees != 0{
+            Parameters["Fees"] = FilterFees
+        }else if Fees != 0{
+            Parameters["Fees"] = Fees
+        }
+        
+        if FilterGenderId != 0{
+            Parameters["GenderId"] = FilterGenderId
+        }else if GenderId != 0{
+            Parameters["GenderId"] = GenderId
+        }
+        
+        if FilterSeniortyLevelId != 0{
+            Parameters["SeniortyLevelId"] = FilterSeniortyLevelId
+        }else if SeniortyLevelId != 0{
+            Parameters["SeniortyLevelId"] = SeniortyLevelId
+        }
+        
+        if FilterSubSpecialistId != []{
+            Parameters["SubSpecialistId"] = FilterSubSpecialistId
+        }else if SubSpecialistId != []{
+            Parameters["SubSpecialistId"] = SubSpecialistId
+        }
+        return .parameterRequest(Parameters: Parameters, Encoding: JSONEncoding.default)
     }
     
     var header: [String : String]? {
-            return ["Authorization":Helper.getAccessToken()]
+        return ["Authorization":Helper.getAccessToken()]
     }
     
-func FetchDoctors(operation:searchDocType){
-    searchDocOperation = operation
-
-    if Helper.isConnectedToNetwork(){
-        self.isLoading = true
-
-        BaseNetwork.request(Target: self, responseModel: BaseResponse<ModelDoc<[Doc]>>.self) { [self] (success, model, err) in
-            if success{
-                //case of success
-                DispatchQueue.main.async {
-                    self.ModelFetchDoctors.send( model!  )
-                }
-
-            }else{
-                if model != nil{
-                    //case of model with error
-                    message = model?.message ?? "Bad Request"
-                    isAlert = true
-                }else{
-                    if err == "Unauthorized"{
-                        //case of Empty model (unauthorized)
-                       message = "Session_expired\nlogin_again".localized(language)
-                                activeAlert = .unauthorized
-                    }else{
-                        isAlert = true
-                        message = err ?? "there is an error"
-                                activeAlert = .serverError
+    func FetchDoctors(operation:searchDocType){
+        searchDocOperation = operation
+        if Helper.isConnectedToNetwork(){
+            self.isLoading = true
+            BaseNetwork.request(Target: self, responseModel: BaseResponse<ModelDoc<[Doc]>>.self) { [self] (success, model, err) in
+                if success{
+                    //case of success
+                    DispatchQueue.main.async {
+                        self.ModelFetchDoctors.send( model!  )
                     }
+                }else{
+                    if model != nil{
+                        //case of model with error
+                        message = model?.message ?? "Bad Request"
+                        isAlert = true
+                    }else{
+                        if err == "Unauthorized"{
+                            //case of Empty model (unauthorized)
+                            message = "Session_expired\nlogin_again".localized(language)
+                            activeAlert = .unauthorized
+                        }else{
+                            isAlert = true
+                            message = err ?? "there is an error"
+                            activeAlert = .serverError
+                        }
+                    }
+                    //                isAlert = true
                 }
-//                isAlert = true
+                isLoading = false
             }
-            isLoading = false
+            
+        }else{
+            //case of no internet connection
+            //        activeAlert = .NetworkError
+            message = "Check_Your_Internet_Connection".localized(language)
+            isAlert = true
         }
-
-    }else{
-        //case of no internet connection
-//        activeAlert = .NetworkError
-        message = "Check_Your_Internet_Connection".localized(language)
-        isAlert = true
     }
-
-}
-
+    
 }
